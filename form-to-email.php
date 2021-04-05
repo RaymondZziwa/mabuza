fc8762586785342000edc9042439ee297cde297d
Raymond Zziwa <46450094+fabricray256@users.noreply.github.com>
2021-04-05 16:24:22 +0300

    Add files via upload

--- form-to-email.php
+++ form-to-email.php
@@ -1,5 +1,49 @@
 <?php
+// Uncomment the next line if you're using a dependency loader (such as Composer) (recommended)
+require 'vendor/autoload.php';
+$API_KEY ="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";
+// Uncomment the next line if you're not using a dependency loader (such as Composer), replacing <PATH TO> with the path to the sendgrid-php.php file
+// require_once '<PATH TO>/sendgrid-php.php';
+
 if(isset($_POST['submit']))
+{
+  $name = $_POST['fname'];
+  $user_Mail = $_POST['email'];
+  $mail = $_POST['text'];
+  
+
+  /*$mailTo = "Mabuzawomenclinic@hotmail.com";
+  $headers = "From: Mabuza Hospital Website";
+  $txt = "You have recieved an email from ".$name.".\n\n".$mail;
+
+  mail($mailTo,$txt,$headers);
+  header("Location: index.php?mailsend");*/
+  $email = new \SendGrid\Mail\Mail();
+  $email->setFrom("Mabuzawomenclinic@hotmail.com", "Mabuza Clinic");
+  $email->setSubject("Inquiry from potential client");
+  $email->addTo($user_Mail, $name);
+  $email->addContent("text/plain", $mail);
+  /*$email->addContent(
+      "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+  );*/
+  $sendgrid = new \SendGrid($API_KEY);
+  if($sendgrid->send($email));
+  {
+    echo "Email sent successfully";
+  }
+  
+  /*try {
+      $response = 
+      print $response->statusCode() . "\n";
+      print_r($response->headers());
+      print $response->body() . "\n";
+  } catch (Exception $e) {
+      echo 'Caught exception: '. $e->getMessage() ."\n";
+  }*/
+}
+
+
+/*if(isset($_POST['submit']))
 {
   $name = $_POST['fname'];
   $user_Mail = $_POST['email'];
@@ -10,10 +54,10 @@ if(isset($_POST['submit']))
   $txt = "You have recieved an email from ".$name.".\n\n".$mail;
 
   mail($mailTo,$txt,$headers);
-  header("Location: index.php?mailsent");
+  header("Location: index.php?mailsend");
 }
 // Function to validate against any email injection attempts
-/*function IsInjected($str)
+function IsInjected($str)
 {
   $injections = array('(\n+)',
               '(\r+)',
