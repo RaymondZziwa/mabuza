3ca7951151f44f2b398bcc71c9e6b75429b70527
Raymond Zziwa <46450094+fabricray256@users.noreply.github.com>
2021-04-05 12:08:09 +0300

    Add files via upload

--- form-to-email.php
+++ form-to-email.php
@@ -4,13 +4,12 @@ if(!isset($_POST['submit']))
 	//This page should not be accessed directly. Need to submit the form.
 	echo "error; you need to submit the form!";
 }
-$firstName = $_POST['fname'];
-$lastName = $_POST['lname'];
+$name = $_POST['fname'];
 $visitor_email = $_POST['email'];
 $message = $_POST['text'];
 
 //Validate first
-if(empty($firstname)||empty($visitor_email)||empty($lastName)) 
+if(empty($name)||empty($visitor_email)) 
 {
     echo "Name and email are mandatory!";
     exit;
@@ -22,17 +21,16 @@ if(IsInjected($visitor_email))
     exit;
 }
 
-$email_from = "mabuzawomenclinic@gmail.com";//<== update the email address
+$email_from = "Mabuza hospital Website";//<== update the email address
 $email_subject = "New Form submission";
-$email_body = "You have received a new message from the user ".$name.".\n".
-    "Here is the message:\n .$message".
+$email_body = "You have received a new message from the user $name.\n".
+    "Here is the message:\n $message".
     
-$to = "raymondzian@gmail.com";//<== update the email address
-$headers = "From: Mabuza website \r\n";
+$to = "mabuzawomenclinic@hotmail.com";//<== update the email address
+$headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
 mail($to,$email_subject,$email_body,$headers);
-
 
 // Function to validate against any email injection attempts
 function IsInjected($str)
@@ -56,4 +54,5 @@ function IsInjected($str)
     return false;
   }
 }
+   
 ?> 
